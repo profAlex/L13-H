e2aa75c3a4e6a469bb941c8091440643700dd2fe
Alex <steblyakov.aleksandr@gmail.com>
2026-04-22 18:12:29 +0300

    Lesson 13 - nestjs first steps - added dotenv config for security

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TestingModule } from './modules/testing/testing.module';
 import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-platform.module';
 import { CoreModule } from './core/core.module';
+import {envConfig} from "./config";
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-bloggers-platform'), //TODO: move to env. will be in the following lessons
+    MongooseModule.forRoot(envConfig.mongoURI), //TODO: move to env. will be in the following lessons
     UserAccountsModule, //все модули должны быть заимпортированы в корневой модуль, либо напрямую, либо по цепочке (через другие модули)
     TestingModule,
     BloggersPlatformModule,
