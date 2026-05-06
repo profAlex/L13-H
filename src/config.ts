e206d47f58ef1d508208fcac90500135f22b9f75
Alex <steblyakov.aleksandr@gmail.com>
2026-05-06 22:47:04 +0300

    Lesson 13 - preparing for platform-testing

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ const DEFAULT_MAIL_HOST = "smtp.yandex.ru";
 const DEFAULT_MAIL_LOGIN = "geniusb198";
 const DEFAULT_MAIL_PASS = "ywzsvigfobsbmqsp";
 
-const DEFAULT_MONGO_URI = "";
+const DEFAULT_MONGO_URI = "mongodb+srv://bolly198:qWvHpN17UfbbBDGI@newlearningcluster.1tnov1c.mongodb.net/?appName=NewLearningCluster";
 
 
 
